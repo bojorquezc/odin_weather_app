0ce3320552055189ea425e28ee68adb9f551a981
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-11-10 22:07:23 -0800

    Resolve deleted image issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 import './style.css';
-import logo from './assets/images/cloudy.png';
 import { getCurrentWeather, getForecastWeather } from './weather_logic';
 
 // console.log(fetchedLocation);
